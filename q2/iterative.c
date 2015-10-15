fe98688d2bbdbd0955d4b7c8150db83c4eb2f3c4
zeroplusone <a711186@gmail.com>
2015-10-16 04:34:38 +0800

    Format q2/iterative.c by astyle
    Install astyle to check coding style.

--- q2/iterative.c
+++ q2/iterative.c
@@ -5,23 +5,26 @@
 #define STR_LENGTH 10
 
 /* produce test case */
-int compare(const void* ch1, const void *ch2) {
+int compare(const void* ch1, const void *ch2)
+{
     return *(char*)ch1 - *(char*)ch2;
 }
 
-char* createStr(char str[]) {
+char* createStr(char str[])
+{
     int i;
-    for(i=0;i<STR_LENGTH;++i) {
+    for(i=0; i<STR_LENGTH; ++i) {
         str[i] = 'a' + rand()%26;
     }
     qsort((void*)str,STR_LENGTH,sizeof(char),compare);
     return str;
 }
 
-char smallest_character(char str[],char c) {
+char smallest_character(char str[],char c)
+{
 
     int i;
-    for(i=0;i<STR_LENGTH;++i) {
+    for(i=0; i<STR_LENGTH; ++i) {
         if(str[i]>c)
             return str[i];
     }
