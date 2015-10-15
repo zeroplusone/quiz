08f334f93000c16870ec674ae2b9df9e630b1a35
zeroplusone <a711186@gmail.com>
2015-10-16 04:21:09 +0800

    Implement q2/interative.c by naive algorithm
    Use for loop to find the smallest char.

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,44 @@
-/* FIXME: Implement! */
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <string.h>
+#define STR_LENGTH 10
+
+/* produce test case */
+int compare(const void* ch1, const void *ch2) {
+    return *(char*)ch1 - *(char*)ch2;
+}
+
+char* createStr(char str[]) {
+    int i;
+    for(i=0;i<STR_LENGTH;++i) {
+        str[i] = 'a' + rand()%26;
+    }
+    qsort((void*)str,STR_LENGTH,sizeof(char),compare);
+    return str;
+}
+
+char smallest_character(char str[],char c) {
+
+    int i;
+    for(i=0;i<STR_LENGTH;++i) {
+        if(str[i]>c)
+            return str[i];
+    }
+    return str[0];
+}
+
+
 
 int main()
 {
+    char str[STR_LENGTH];
+
+    /* create test case */
+    createStr(str);
+    char c = 'a'+rand()%26;
+
+    smallest_character(str,c);
+
     return 0;
 }
